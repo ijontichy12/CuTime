241d40af2bc70c4c09622592b05c7b3d51539117
ijontichy12 <mbenther@gmail.com>
2025-01-18 01:37:48 +0100

    cleaned the dashboard

--- main.py
+++ main.py
@@ -219,13 +219,18 @@ def dashboard():
     work_times = []
     for employee in employees:
         work_times.extend(WorkTime.query.filter_by(employee_id=employee.id).all())
-    # Group work times by date
+    
+    # Group work times by date and sort by date
     work_times_by_date = {}
     for worktime in work_times:
         date_str = worktime.date.strftime('%d.%m.%Y')
         if date_str not in work_times_by_date:
             work_times_by_date[date_str] = []
         work_times_by_date[date_str].append(worktime)
+    
+    # Sort the dictionary by date (ascending order)
+    work_times_by_date = dict(sorted(work_times_by_date.items(), key=lambda item: datetime.strptime(item[0], '%d.%m.%Y')))
+    
     return render_template('dashboard.html', employees=employees, work_times_by_date=work_times_by_date)
 
 @app.route('/add_employee', methods=['GET', 'POST'])
